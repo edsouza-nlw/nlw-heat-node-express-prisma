1e03640ddb99ef78cf8c7a72f133caa60295767c
edrajao <4ucode@gmail.com>
2021-10-19 22:23:40 -0300

    Inicial commit

---
+++ src/controllers/CreateMessageController.ts
@@ -0,0 +1,13 @@
+import {Request, Response} from "express";
+// import {CreateMessageService} from "../services/CreateMessageService";
+
+
+class CreateMessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body;
+
+
+  }
+}
+
+export {CreateMessageController};
